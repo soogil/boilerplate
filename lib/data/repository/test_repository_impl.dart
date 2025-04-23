a5d2eb95d972630f28e52d6494583f06bc519bf4
soogil <zmfflrql5@gmail.com>
2025-04-23 17:22:22 +0900

    폴더 구조 정리

---
+++ lib/data/repository/test_repository_impl.dart
@@ -0,0 +1,7 @@
+import 'package:boilerplate/domain/repository/test_repository.dart';
+
+
+// 모델에서 엔티티로 변환 로직 및 인터페이스 구체적 구현
+class TestRepositoryImpl extends TestRepository {
+
+}
---
+++ lib/domain/repository/test_repository.dart
@@ -0,0 +1,7 @@
+
+
+
+// 반환 타입은 엔티티로 사용
+abstract class TestRepository {
+
+}
