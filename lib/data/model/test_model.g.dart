a8dc2f36eae3c68ac375c96e8e6bdca74de5dd11
soogil <zmfflrql5@gmail.com>
2025-05-15 20:58:55 +0900

    model entity 코드 추가

---
+++ lib/data/model/test_model.g.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'test_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+TestModel _$TestModelFromJson(Map<String, dynamic> json) => TestModel(
+      id: (json['id'] as num).toInt(),
+    );
+
+Map<String, dynamic> _$TestModelToJson(TestModel instance) => <String, dynamic>{
+      'id': instance.id,
+    };
---
+++ lib/domain/entity/test_entity.dart
@@ -0,0 +1,6 @@
+import 'package:boilerplate/data/model/test_model.dart';
+
+
+class TestEntity extends TestModel{
+  TestEntity({required super.id});
+}
