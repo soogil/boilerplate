ddd3b5af6c6e8ddb326414026fb58a72948f35f8
soogil <zmfflrql5@gmail.com>
2025-05-15 20:09:48 +0900

    Test api 코드 및 repository impl 코드 추가

--- lib/data/repository/test_repository_impl.dart
+++ lib/data/repository/test_repository_impl.dart
@@ -1,7 +1,16 @@
+import 'package:boilerplate/core/api/test_api.dart';
 import 'package:boilerplate/domain/repository/test_repository.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
+
+final testRepository = Provider<Future<TestRepository>>((ref) async {
+  final api = await ref.read(testApiProvider);
+  return TestRepositoryImpl(api);
+});
 
 // 모델에서 엔티티로 변환 로직 및 인터페이스 구체적 구현
 class TestRepositoryImpl extends TestRepository {
+  TestRepositoryImpl(this._api);
 
+  final TestApi _api;
 }
