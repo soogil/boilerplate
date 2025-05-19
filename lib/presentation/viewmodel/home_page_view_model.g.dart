99a4eb49e400281d16a8ca9455664a0b3fcb78b8
soogil <zmfflrql5@gmail.com>
2025-05-19 21:23:45 +0900

    viewmodel .g.dart 파일 추가

---
+++ lib/presentation/viewmodel/home_page_view_model.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'home_page_view_model.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$mainPageViewModelHash() => r'361e7115d7402bba1dff4b6ece22798c9a6cf650';
+
+/// See also [MainPageViewModel].
+@ProviderFor(MainPageViewModel)
+final mainPageViewModelProvider =
+    AutoDisposeNotifierProvider<MainPageViewModel, void>.internal(
+  MainPageViewModel.new,
+  name: r'mainPageViewModelProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$mainPageViewModelHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$MainPageViewModel = AutoDisposeNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
