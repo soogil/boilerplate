165ac4d05a976eff2531809893f4a27465369b51
soogil <zmfflrql5@gmail.com>
2025-05-19 21:13:59 +0900

    viewmodel 코드 추가

--- lib/presentation/viewmodel/home_page_view_model.dart
+++ lib/presentation/viewmodel/home_page_view_model.dart
@@ -1,10 +1,11 @@
-//
-// part 'main_page_view_model.g.dart';
-//
-// @riverpod
-// class MainPageViewModel extends _$MainPageViewModel {
-//
-//   @override
-//   void build() {
-//   }
-// }
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'home_page_view_model.g.dart';
+
+@riverpod
+class MainPageViewModel extends _$MainPageViewModel {
+
+  @override
+  void build() {
+  }
+}
